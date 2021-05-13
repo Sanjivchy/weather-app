68546f1605fca6583d63a8cc7a608be685806aae
xoonziip <sanjivchaudhary416@gmail.com>
2021-05-13 23:29:48 +0545

    data fetched from api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ const api ={
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result));
+    }
+  }
+
   const dateBuilder =(d) =>{
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -35,7 +45,7 @@ function App() {
           <div className="temp">
             15C
           </div>
-          <div className="weater">
+          <div className="weather">
             Sunny
           </div>
         </div>     
